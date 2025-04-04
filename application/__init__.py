3681f07e1bacb5da498086a23a0ad15286ce1ff6
student <Mbarraza94@gmail.com>
2025-04-04 16:31:10 -0700

    added register route

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,9 @@
 import os
 from unicodedata import category
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, session
 from flaskext.mysql import MySQL
+import re
 
 from markupsafe import escape
 
@@ -15,8 +16,7 @@ app.config['MYSQL_DATABASE_DB'] = 'TestDb'
 app.config['MYSQL_DATABASE_HOST'] = '18.222.76.244'
 mysql = MySQL()
 mysql.init_app(app)
-conn = mysql.connect()
-cursor = conn.cursor()
+
 
 # Home Page
 @app.route("/")
@@ -38,6 +38,9 @@ def team_member(name):
 def search():
     if request.method == "GET":
         return redirect('/')
+    
+    conn = mysql.connect()
+    cursor = conn.cursor()
     
     # parse form input
     selected_filter = request.form.get('filter', '').strip()
@@ -91,8 +94,62 @@ def search():
     data = cursor.fetchall()
 
     print('Result:', *data, sep='\n')
+
+    cursor.close()
+    conn.close()
+
     return render_template('index.html', data=data)
     
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     msg = ''
+#     conn = mysql.connect()  # create new connection
+#     cursor = conn.cursor()
+#     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+#         username = request.form['username']
+#         password = request.form['password']
+#         email = request.form['email']
+
+#         cursor.execute('SELECT * FROM Account WHERE username = %s', (username,))
+#         account = cursor.fetchone()
+#         if account:
+#             msg = 'Account already exists!'
+#         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+#             msg = 'Invalid email address!'
+#         elif not re.match(r'[A-Za-z0-9]+', username):
+#             msg = 'Username must contain only letters and numbers!'
+#         elif not username or not password or not email:
+#             msg = 'Please fill out the form!'
+#         else:
+#             # Generate the next idUser and idAccount or need to auto increment in database schema
+#             cursor.execute("SELECT MAX(idUser) FROM User")
+#             max_user_id = cursor.fetchone()[0] or 0
+#             new_id_user = max_user_id + 1
+
+#             cursor.execute("SELECT MAX(idAccount) FROM Account")
+#             max_account_id = cursor.fetchone()[0] or 0
+#             new_id_account = max_account_id + 1
+
+#             # 3. Insert into User since Account has foreign key to User
+#             test_dob = "2000-01-01"  # or use a default string
+#             cursor.execute("INSERT INTO User (idUser, name, DOB) VALUES (%s, %s, %s)", (new_id_user, username, test_dob))
+
+#             # 4. Insert into Account table
+#             cursor.execute('''
+#                 INSERT INTO Account (idUser, idAccount, username, hashed_password, email)
+#                 VALUES (%s, %s, %s, %s, %s)
+#             ''', (new_id_user, new_id_account, username, password, email))
+
+#             conn.commit()
+#             msg = 'You have successfully registered!'
+
+
+#     cursor.close()
+#     conn.close()  
+#     return render_template('registration.html', msg=msg)
+
+
+
 
 if __name__ == '__main__':
     app.debug = True
