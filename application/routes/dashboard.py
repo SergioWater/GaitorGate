e16c9b1dfba8cec1440359a05424cd6e4955f389
student <Mbarraza94@gmail.com>
2025-05-04 11:22:36 -0700

    Updated dashboard to have relevant user info

--- application/routes/dashboard.py
+++ application/routes/dashboard.py
@@ -5,29 +5,52 @@ import MySQLdb.cursors
 
 dashboard_bp = Blueprint('dashboard', __name__)
 
-@dashboard_bp.route('/dashboard', methods=['GET', 'POST'])
+@dashboard_bp.route('/dashboard')
 @login_required
 def dashboard():
     with current_app.app_context():  # <-- Add this context manager
-        if request.method == "POST":
-            conn = current_app.config['MYSQL'].connection
-            cursor = conn.cursor(MySQLdb.cursors.DictCursor)
-            id_user = current_user.id
-            # Check Users Account Type
-            cursor.execute("Select Account_Type FROM Account WHERE idAccount = %s", (id_user,))
-            account_type = cursor.fetchone()['Account_Type']
-            if account_type != 'Company':
-                # For types that are not of Company return tools with rating of 4 stars or greater
-                cursor.execute("SELECT Tools.name , Avg(Rating.rating), Tools.url, " \
-                "Tools.thumbnail_url, Tools.version, Tools.pricing, Tools.description " \
-                "FROM SearchIndex JOIN Rating ON SearchIndex.idIndex = Rating.idIndex " \
-                "JOIN Tools ON SearchIndex.idTool = Tools.idTool GROUP BY Tools.idTool " \
-                "HAVING Avg(Rating.rating) >= 4")
-                results = cursor.fetchall()
-            else:
-                #For users who are of type Company return search history organized by date
-                cursor.execute("SELECT * FROM Search_History ORDER BY created_at DESC")
-                results = cursor.fetchall()
+        conn = current_app.config['MYSQL'].connection
+        cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+        id_user = current_user.id
+        # Check Users Account Type
+        cursor.execute("Select Account_Type FROM Account WHERE idAccount = %s", (id_user,))
+        account_type = cursor.fetchone()['Account_Type']
+        if account_type == 'Company':
+            # Display summary of Company's tool
+            cursor.execute("SELECT Company.company_name, Tools.name, " \
+            "Company.website, Tools.url, Tools.thumbnail_url, Tools.version, " \
+            "Tools.pricing, Tools.description " \
+            "FROM Account " \
+            "JOIN Company ON Account.idAccount = Company.idAccount " \
+            "JOIN Tools ON Company.idAccount = Tools.company " \
+            "WHERE Account.idUser = %s", (id_user,))
+            tool_summary_results = cursor.fetchall()
 
+            # Get reviews that were posted for this company 
+            cursor.execute("SELECT Tools.name, Review.review_text, Review_created_at " \
+            "FROM Account " \
+            "JOIN Company ON Account.idAccount = Company.idAccount " \
+            "JOIN Tools ON Company.idAccount = Tools.company " \
+            "JOIN SearchIndex ON Tools.idTool = SearchIndex.idTool " \
+            "JOIN Review ON SearchIndex.idIndex = Review.idIndex " \
+            "WHERE Account.idUser = %s " \
+            "ORDER BY Review.created_at DESC", (id_user,))
+            review_results = cursor.fetchall()
+            
 
-        return render_template('dashboard.html', title='Dashboard', account_type=account_type, results=results)
+        else:
+            #Display reviews that users posted
+            cursor.execute("SELECT Review.idAccount, Review.review_text, Review.created_at " \
+            "FROM Account JOIN Review ON Account.idAccount = Review.idAccount " \
+            "WHERE idUser = %s ", (id_user,))
+            review_results = cursor.fetchall()
+            
+        cursor.close()
+
+        return render_template(
+            'index.html',
+            title='Dashboard',
+            account_type=account_type,
+            tool_summary=tool_summary_results if account_type == 'Company' else None,
+            reviews=review_results
+        )
